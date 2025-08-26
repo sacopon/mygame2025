eb4c8536f17e6a383011b7650c0826d7e7e70d27
sacopon <sacopon.dev@gmail.com>
2025-08-26 22:32:34 +0900

    縦/横対応

--- src/main.ts
+++ src/main.ts
@@ -1,116 +1,200 @@
 import "./index.css";
-import { Application, Assets, Container, Graphics, SCALE_MODES, Sprite, Spritesheet } from "pixi.js";
+import { Application, Assets, Container, Graphics, SCALE_MODES, Sprite, Spritesheet, Texture } from "pixi.js";
 
 // ゲーム画面の内部サイズ
 const GAME_SCREEN_WIDTH = 256;
 const GAME_SCREEN_HEIGHT = 224;
 
 // 仮想ゲーム機の設定
-const virtualConsoleUiSetting = {
-  // 仮想ゲーム機の大きさ
-  body: {
-    size: {
-      width: 720,
-      height: 1280,
+const skins = [
+  // 縦画面用
+  {
+    // 仮想ゲーム機の大きさ
+    body: {
+      size: {
+        width: 720,
+        height: 1280,
+      },
+      image: "v_body.png",
     },
-    image: "body.png",
-  },
-  // 仮想ゲーム画面
-  screen: {
-    // 大きさ
-    size: {
-      width: 432,
-      height: 378,
-    },
-    // 位置
-    position: {
-      x: 152 -  720 / 2,
-      y: 200 - 1280 / 2,
-    },
-  },
-  // キーの定義
-  key: {
-    // 方向キー
-    direction: {
+    // 仮想ゲーム画面
+    screen: {
+      // 大きさ
+      size: {
+        width: 432,
+        height: 378,
+      },
+      // 位置
       position: {
-        x: 150,
-        y: 830,
-      },
-      image: {
-        neutral: "dir_neutral.png",
-        up: "dir_up.png",
-        down: "dir_down.png",
-        left: "dir_left.png",
-        right: "dir_right.png",
+        x: 152 -  720 / 2,
+        y: 200 - 1280 / 2,
       },
     },
-    // その他のボタン
-    buttons: [
-      // ボタン1(Aボタン)
-      {
+    // キーの定義
+    key: {
+      // 方向キー
+      direction: {
         position: {
-          x: 628,
-          y: 798,
+          x: 150,
+          y: 830,
         },
         image: {
-          on: "circle_button_on.png",
-          off: "circle_button_off.png",
+          neutral: "v_dir_neutral.png",
+          up: "v_dir_up.png",
+          down: "v_dir_down.png",
+          left: "v_dir_left.png",
+          right: "v_dir_right.png",
         },
       },
-      // ボタン2(Bボタン)
-      {
-        position: {
-          x: 518,
-          y: 844,
+      // その他のボタン
+      buttons: [
+        // ボタン1(Aボタン)
+        {
+          position: {
+            x: 628,
+            y: 798,
+          },
+          image: {
+            on: "v_circle_button_on.png",
+            off: "v_circle_button_off.png",
+          },
         },
-        image: {
-          on: "circle_button_on.png",
-          off: "circle_button_off.png",
+        // ボタン2(Bボタン)
+        {
+          position: {
+            x: 518,
+            y: 844,
+          },
+          image: {
+            on: "v_circle_button_on.png",
+            off: "v_circle_button_off.png",
+          },
         },
-      },
-      // ボタン3(STARTボタン)
-      {
-        position: {
-          x: 378,
-          y: 982,
+        // ボタン3(STARTボタン)
+        {
+          position: {
+            x: 378,
+            y: 982,
+          },
+          image: {
+            on: "v_rect_button_on.png",
+            off: "v_rect_button_off.png",
+          },
         },
-        image: {
-          on: "rect_button_on.png",
-          off: "rect_button_off.png",
-        },
-      },
-      // ボタン4(SELECTボタン)
-      {
-        position: {
-          x: 262,
-          y: 982,
-        },
-        image: {
-          on: "rect_button_on.png",
-          off: "rect_button_off.png",
-        },
-      }
-    ],
+        // ボタン4(SELECTボタン)
+        {
+          position: {
+            x: 262,
+            y: 982,
+          },
+          image: {
+            on: "v_rect_button_on.png",
+            off: "v_rect_button_off.png",
+          },
+        }
+      ],
+    },
   },
-};
+  // 横画面用
+  {
+    // 仮想ゲーム機の大きさ
+    body: {
+      size: {
+        width: 1280,
+        height: 720,
+      },
+      image: "h_body.png",
+    },
+    // 仮想ゲーム画面
+    screen: {
+      // 大きさ
+      size: {
+        width:  410,
+        height: 360,
+      },
+      // 位置
+      position: {
+        x: 428 - 1280 / 2,
+        y:  84 -  720 / 2,
+      },
+    },
+    // キーの定義
+    key: {
+      // 方向キー
+      direction: {
+        position: {
+          x: 178,
+          y: 256,
+        },
+        image: {
+          neutral: "h_dir_neutral.png",
+          up: "h_dir_up.png",
+          down: "h_dir_down.png",
+          left: "h_dir_left.png",
+          right: "h_dir_right.png",
+        },
+      },
+      // その他のボタン
+      buttons: [
+        // ボタン1(Aボタン)
+        {
+          position: {
+            x: 1120,
+            y:  220,
+          },
+          image: {
+            on: "h_circle_button_on.png",
+            off: "h_circle_button_off.png",
+          },
+        },
+        // ボタン2(Bボタン)
+        {
+          position: {
+            x: 1046,
+            y:  300,
+          },
+          image: {
+            on: "h_circle_button_on.png",
+            off: "h_circle_button_off.png",
+          },
+        },
+        // ボタン3(STARTボタン)
+        {
+          position: {
+            x: 1048,
+            y:  130,
+          },
+          image: {
+            on: "h_start_button_on.png",
+            off: "h_start_button_off.png",
+          },
+        },
+      ],
+    },
+  }
+];
 
-function buildVirtualConsoleUi(setting: any, spriteSheet: Spritesheet, body: Sprite, direction: Sprite, buttons: Sprite[]) {
+let skin = skins[0];
+
+function buildVirtualConsoleUi(setting: any, body: Sprite, direction: Sprite, buttons: Sprite[]) {
   // ゲーム機本体
-  body.texture = spriteSheet.textures[setting.body.image];
+  body.texture = Texture.from(setting.body.image);
   body.position.set(
     setting.body.size.width  / 2,
     setting.body.size.height / 2);
 
   // 方向キー
-  direction.texture = spriteSheet.textures[setting.key.direction.image.neutral];
+  direction.texture = Texture.from(setting.key.direction.image.neutral);
   direction.position.set(
     setting.key.direction.position.x,
     setting.key.direction.position.y);
 
   // その他ボタン
-  buttons.forEach((button, i) => {
-    button.texture = spriteSheet.textures[setting.key.buttons[i].image.off];
-    button.position.set(setting.key.buttons[i].position.x, setting.key.buttons[i].position.y);
+  buttons.forEach(button => button.visible = false);
+  setting.key.buttons.forEach((button: any, i: number) => {
+    buttons[i].texture = Texture.from(button.image.off);
+    buttons[i].position.set(button.position.x, button.position.y);
+    buttons[i].visible = true;
   });
 }
 
@@ -128,7 +212,8 @@ function buildVirtualConsoleUi(setting: any, spriteSheet: Spritesheet, body: Spr
 
   // 画像読み込み
   const bg_texture = await Assets.load("/textures/screen_bg.png");
-  const vpad_sprite_sheet: Spritesheet = await Assets.load("/textures/virticalui.json");
+  await Assets.load("/textures/virticalui.json");
+  await Assets.load("/textures/horizontalui.json");
 
   // 背景
   const bg_sprite = new Sprite(bg_texture);
@@ -143,40 +228,41 @@ function buildVirtualConsoleUi(setting: any, spriteSheet: Spritesheet, body: Spr
   root_container.addChild(ui_layer);
 
   ui_layer.pivot.set(
-    virtualConsoleUiSetting.body.size.width  / 2,
-    virtualConsoleUiSetting.body.size.height / 2);
+    skin.body.size.width  / 2,
+    skin.body.size.height / 2);
 
   // ゲーム機本体(UIレイヤー)
-  const body_sprite = new Sprite(vpad_sprite_sheet.textures[virtualConsoleUiSetting.body.image]);
+  const body_sprite = Sprite.from(skin.body.image);
   body_sprite.anchor.set(0.5);
   ui_layer.addChild(body_sprite);
 
   // 方向キー(UIレイヤー)
-  const direction_pad = new Sprite(vpad_sprite_sheet.textures[virtualConsoleUiSetting.key.direction.image.neutral]);
+  const direction_pad = Sprite.from(skin.key.direction.image.neutral);
   direction_pad.anchor.set(0.5);
   ui_layer.addChild(direction_pad);
 
   const buttons: Sprite[] = [];
 
-  virtualConsoleUiSetting.key.buttons.forEach(button => {
-    const sprite = new Sprite(vpad_sprite_sheet.textures[button.image.off]);
+  for (let i = 0; i < 4; ++i) {
+    const button = skin.key.buttons[i];
+    const is_only_definition = i < skin.key.buttons.length;
+    const sprite = Sprite.from(!is_only_definition ? button.image.off : skin.key.buttons[0].image.off);
     sprite.anchor.set(0.5);
     ui_layer.addChild(sprite);
+    sprite.visible = !is_only_definition;
     buttons.push(sprite);
-  });
+  }
 
-  buildVirtualConsoleUi(virtualConsoleUiSetting, vpad_sprite_sheet, body_sprite, direction_pad, buttons);
+  buildVirtualConsoleUi(skin, body_sprite, direction_pad, buttons);
 
   // ゲーム画面レイヤー
   const game_layer = new Container();
   game_layer.position.set(
-    virtualConsoleUiSetting.screen.position.x,
-    virtualConsoleUiSetting.screen.position.y);
+    skin.screen.position.x,
+    skin.screen.position.y);
   game_layer.pivot.set(0, 0);
-  game_layer.scale.set(virtualConsoleUiSetting.screen.size.width / GAME_SCREEN_WIDTH);
+  game_layer.scale.set(skin.screen.size.width / GAME_SCREEN_WIDTH);
   root_container.addChild(game_layer);
-
-  console.log(Object.keys(vpad_sprite_sheet.textures));
 
   // ゲーム画面内のサンプル描画
   {
@@ -208,46 +294,75 @@ function buildVirtualConsoleUi(setting: any, spriteSheet: Spritesheet, body: Spr
     const cw = window.innerWidth;
     const ch = window.innerHeight;
     app.renderer.resize(cw, ch);
+    skin = (cw < ch) ? skins[0] : skins[1];
 
-    // スケール計算
-    const scale = Math.min(
-      cw / virtualConsoleUiSetting.body.size.width,
-      ch / virtualConsoleUiSetting.body.size.height);
-    root_container.scale.set(scale);
+    // UIレイヤーの pivot を本体画像のサイズに合わせて再設定
+    ui_layer.pivot.set(
+      skin.body.size.width  / 2,
+      skin.body.size.height / 2
+    );
 
     // UI を再配置
-    buildVirtualConsoleUi(virtualConsoleUiSetting, vpad_sprite_sheet, body_sprite, direction_pad, buttons);
+    buildVirtualConsoleUi(skin, body_sprite, direction_pad, buttons);
 
-    // 描画範囲(ウィンドウ全体)の中央に配置
+    // game_layer を現在の skin のスクリーン位置・サイズに合わせ直す
+    game_layer.position.set(skin.screen.position.x, skin.screen.position.y);
+    game_layer.scale.set(skin.screen.size.width / GAME_SCREEN_WIDTH);
+
+    // 全体スケーリングとセンタリング
+    const scale = Math.min(
+      cw / skin.body.size.width,
+      ch / skin.body.size.height);
+    root_container.scale.set(scale);
     root_container.position.set(~~(cw / 2), ~~(ch / 2));
   }
 
   // キーボード入力イベント
   window.addEventListener("keydown", e => {
     if (e.key === "ArrowUp") {
-      direction_pad.texture = vpad_sprite_sheet.textures[virtualConsoleUiSetting.key.direction.image.up];
+      direction_pad.texture = Texture.from(skin.key.direction.image.up);
     } else if (e.key === "ArrowDown") {
-      direction_pad.texture = vpad_sprite_sheet.textures[virtualConsoleUiSetting.key.direction.image.down];
+      direction_pad.texture = Texture.from(skin.key.direction.image.down);
     } else if (e.key === "ArrowLeft") {
-      direction_pad.texture = vpad_sprite_sheet.textures[virtualConsoleUiSetting.key.direction.image.left];
+      direction_pad.texture = Texture.from(skin.key.direction.image.left);
     } else if (e.key === "ArrowRight") {
-      direction_pad.texture = vpad_sprite_sheet.textures[virtualConsoleUiSetting.key.direction.image.right];
+      direction_pad.texture = Texture.from(skin.key.direction.image.right);
     } else if (e.key === "z" || e.key === "Z") {
-      buttons[0].texture = vpad_sprite_sheet.textures[virtualConsoleUiSetting.key.buttons[0].image.on];
+      const buttonIndex = 0;
+
+      if (buttonIndex < skin.key.buttons.length) {
+        buttons[buttonIndex].texture = Texture.from(skin.key.buttons[buttonIndex].image.on);
+      }
     } else if (e.key === "x" || e.key === "X") {
-      buttons[1].texture = vpad_sprite_sheet.textures[virtualConsoleUiSetting.key.buttons[1].image.on];
+      const buttonIndex = 1;
+
+      if (buttonIndex < skin.key.buttons.length) {
+        buttons[buttonIndex].texture = Texture.from(skin.key.buttons[buttonIndex].image.on);
+      }
     } else if (e.key === "a" || e.key === "A") {
-      buttons[2].texture = vpad_sprite_sheet.textures[virtualConsoleUiSetting.key.buttons[2].image.on];
+      const buttonIndex = 2;
+
+      if (buttonIndex < skin.key.buttons.length) {
+        buttons[buttonIndex].texture = Texture.from(skin.key.buttons[buttonIndex].image.on);
+      }
     } else if (e.key === "s" || e.key === "S") {
-      buttons[3].texture = vpad_sprite_sheet.textures[virtualConsoleUiSetting.key.buttons[3].image.on];
+      const buttonIndex = 3;
+
+      if (buttonIndex < skin.key.buttons.length) {
+        buttons[buttonIndex].texture = Texture.from(skin.key.buttons[buttonIndex].image.on);
+      }
     }
   });
 
   window.addEventListener("keyup", (e) => {
-    direction_pad.texture = vpad_sprite_sheet.textures[virtualConsoleUiSetting.key.direction.image.neutral];
+    direction_pad.texture = Texture.from(skin.key.direction.image.neutral);
 
     buttons.forEach((button, i) => {
-      button.texture = vpad_sprite_sheet.textures[virtualConsoleUiSetting.key.buttons[i].image.off];
+      if (skin.key.buttons.length <= i) {
+        return;
+      }
+
+      button.texture = Texture.from(skin.key.buttons[i].image.off);
     });
   });
 
