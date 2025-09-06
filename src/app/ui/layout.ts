2552542da6d18b911eaca263a9e3ffefd57974e4
sacopon <sacopon.dev@gmail.com>
2025-09-06 15:57:21 +0900

    VirtualPad 周りを整理

--- src/app/ui/layout.ts
+++ src/app/ui/layout.ts
@@ -1,53 +1,7 @@
-import { Texture, Sprite, Application } from "pixi.js";
-import { GameScreenSpec, type Skin, type UiContext } from "@/app";
-import { InputState, PAD_BIT } from "@/shared";
+import { Application } from "pixi.js";
+import { GameScreenSpec, type Skin, type AppContext } from "@/app";
 
-export type UIMode = "pad" | "bare";
-
-/**
- * ボタン状況に応じて画像を更新する
- *
- * @param skin 各種画像の定義
- * @param state 入力状況
- * @param directionPad 方向キーのスプライト
- * @param buttons ボタン類のスプライト
- */
-export function updateButtonImages(skin: Skin, state: InputState, directionPad: Sprite, buttons: Sprite[]) {
-  const composed = state.composed();
-
-  // ボタン状況に応じて画像を更新する
-  let directionTexImage = skin.key.direction.image.neutral;
-
-  if (composed & (1 << PAD_BIT.DPAD_UP)) {
-    directionTexImage = skin.key.direction.image.up;
-  }
-  else if (composed & (1 << PAD_BIT.DPAD_DOWN)) {
-    directionTexImage = skin.key.direction.image.down;
-  }
-  else if (composed & (1 << PAD_BIT.DPAD_LEFT)) {
-    directionTexImage = skin.key.direction.image.left;
-  }
-  else if (composed & (1 << PAD_BIT.DPAD_RIGHT)) {
-    directionTexImage = skin.key.direction.image.right;
-  }
-
-  const directionTexture = Texture.from(directionTexImage);
-  if (directionPad.texture !== directionTexture) {
-    directionPad.texture = directionTexture;
-  }
-
-  // A/B/START/SELECT ボタン
-  for (let i = 0; i < 4; ++i) {
-    if (skin.key.buttons.length <= i) {
-      break;
-    }
-
-    const bit = PAD_BIT.BUTTON1 + i;
-    buttons[i].texture = Texture.from((composed & (1 << bit)) ? skin.key.buttons[i].image.on : skin.key.buttons[i].image.off);
-  }
-}
-
-export function relayoutViewport(app: Application, ctx: UiContext, gameScreenSpec: GameScreenSpec, skin: Skin, w: number, h: number) {
+export function relayoutViewport(app: Application, ctx: AppContext, gameScreenSpec: GameScreenSpec, skin: Skin, w: number, h: number) {
   const cw = (app.renderer.canvas as HTMLCanvasElement).width;
   const ch = (app.renderer.canvas as HTMLCanvasElement).height;
 
@@ -69,7 +23,7 @@ export function relayoutViewport(app: Application, ctx: UiContext, gameScreenSpe
   ctx.background.position.set(w / 2, h / 2);
 }
 
-export function relayoutViewportBare(app: Application, ctx: UiContext, gameScreenSpec: GameScreenSpec, w: number, h: number, pixelPerfect = false) {
+export function relayoutViewportBare(app: Application, ctx: AppContext, gameScreenSpec: GameScreenSpec, w: number, h: number, pixelPerfect = false) {
   const cw = (app.renderer.canvas as HTMLCanvasElement).width;
   const ch = (app.renderer.canvas as HTMLCanvasElement).height;
 
