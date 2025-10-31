365bb88ade7228228364f04e55488397a06ccb21
sacopon <sacopon.dev@gmail.com>
2025-11-01 07:06:43 +0900

    safe area

--- src/app/lifecycle/runtime/setup-ui-layers-and-controls.ts
+++ src/app/lifecycle/runtime/setup-ui-layers-and-controls.ts
@@ -2,12 +2,15 @@ import { Sprite } from "pixi.js";
 import { toggleMode } from "./runtime-actions";
 import { RuntimeContext } from "./runtime-context";
 import { ScreenTouchHandler, ToggleButton, UIMODE, VirtualPadUI, VirtualPadUIForBare } from "../..";
+import { getSafeAreaInsets } from "@core/browser/get-safe-area-insets";
 
 export function setupUiLayersAndControls(rc: RuntimeContext): void {
   // 背景作成
   const bg = Sprite.from("screen_bg.png");
   bg.anchor.set(0.5);
   rc.layers.background.addChild(bg);
+
+  const safeAreaInsets = getSafeAreaInsets();
 
   // サウンドのミュートON/OFFボタン
   const soundMuteToggleButton = new ToggleButton({ on: "soundon64.png", off: "soundoff64.png" }, false, () => {
@@ -16,13 +19,14 @@ export function setupUiLayersAndControls(rc: RuntimeContext): void {
     return rc.audio.isMuted;
   });
   rc.layers.appUiLayer.addChild(soundMuteToggleButton);
-  soundMuteToggleButton.position.set(soundMuteToggleButton.width / 2, soundMuteToggleButton.height / 2);
+  soundMuteToggleButton.position.set(safeAreaInsets.left + soundMuteToggleButton.width / 2, safeAreaInsets.top + soundMuteToggleButton.height / 2);
 
   // ベアモードON/OFFボタン
-  // TODO: 最初のモードを反映する
   const bareModeToggleButton = new ToggleButton({ on: "fullscreenon64.png", off: "fullscreenoff64.png" }, rc.mode === UIMODE.BARE, () => toggleMode(rc));
   rc.layers.appUiLayer.addChild(bareModeToggleButton);
-  bareModeToggleButton.position.set(soundMuteToggleButton.width + bareModeToggleButton.width / 2 + 24, bareModeToggleButton.height / 2);
+  bareModeToggleButton.position.set(
+    soundMuteToggleButton.x + Math.floor(soundMuteToggleButton.width / 2) + 24 + bareModeToggleButton.width / 2,
+    soundMuteToggleButton.y - Math.floor(soundMuteToggleButton.height / 2) + Math.floor(bareModeToggleButton.height / 2));
 
   rc.padUI = VirtualPadUI.attach(rc.layers, rc.skins.current, rc.inputState);
   rc.bareUI = new VirtualPadUIForBare(rc.inputState, { width: window.innerWidth, height: window.innerHeight });
